c901dc39ffa1578c59cdac3e86dc180a76fc49f1
Varun Mehta <varun.mehta@hey.com>
2021-02-17 00:51:25 +0530

    Add Introslider logic along with PreferencesManager.kt to manange SharedPreferences

---
+++ app/src/main/java/com/android/sealanesshipping/WelcomeActivity.kt
@@ -0,0 +1,69 @@
+package com.android.sealanesshipping
+
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.github.paolorotolo.appintro.AppIntro2
+import com.github.paolorotolo.appintro.AppIntro2Fragment
+import com.github.paolorotolo.appintro.model.SliderPagerBuilder
+
+class WelcomeActivity : AppIntro2() {
+    private lateinit var manager: PreferencesManager
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        manager = PreferencesManager(this)
+        if (manager.isFirstRun()) {
+            showIntroSlides()
+        } else {
+            goToMain()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showIntroSlides() {
+        manager.setFirstRun()
+        val pageOne = SliderPagerBuilder()
+            .title(getString(R.string.slide_one_top_text))
+            .description(getString(R.string.slide_one_down_text))
+            .imageDrawable(R.drawable.slider1)
+            .build()
+
+        val pageTwo = SliderPagerBuilder()
+            .title(getString(R.string.slide_two_top_text))
+            .description(getString(R.string.slide_two_down_text))
+            .imageDrawable(R.drawable.slider2)
+            .build()
+
+        val pageThree = SliderPagerBuilder()
+            .title(getString(R.string.slide_three_top_text))
+            .description(getString(R.string.slide_three_down_text))
+            .imageDrawable(R.drawable.slider3)
+            .build()
+
+        addSlide(AppIntro2Fragment.newInstance(pageOne))
+        addSlide(AppIntro2Fragment.newInstance(pageTwo))
+        addSlide(AppIntro2Fragment.newInstance(pageThree))
+        showStatusBar(false)
+        setProgressIndicator()
+        setImmersiveMode(true)
+    }
+
+    private fun goToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    override fun onSkipPressed(currentFragment: Fragment?) {
+        super.onSkipPressed(currentFragment)
+        goToMain()
+    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        goToMain()
+    }
+}
