b14a80f99890c443419ad6e223e4e8fd9c502d6b
Varun Mehta <varun.mehta@hey.com>
2021-02-18 15:57:27 +0530

    Add SuccessFragment.kt, WeightInput.kt and DestinationFragment.kt. Finish their layouts. Add string resources in strings.xml

--- app/src/main/java/com/android/sealanesshipping/SuccessFragment.kt
+++ app/src/main/java/com/android/sealanesshipping/SuccessFragment.kt
@@ -1,19 +1,19 @@
 package com.android.sealanesshipping
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 
-class WeightInput : Fragment() {
+class SuccessFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weight_input, container, false)
+        return inflater.inflate(R.layout.fragment_success, container, false)
     }
 
 }
